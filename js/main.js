4904d8a3ea4f2015c00d2dc66aec4304895c2961
Valentina <34088724+htmlcoderexe@users.noreply.github.com>
2025-07-12 19:34:09 +0200

    working ajax-based autocomplete/suggestionbox with keyboard and mouse/touch usage

--- js/main.js
+++ js/main.js
@@ -106,17 +106,114 @@ function DoPostUpdate()
     ajax.send("property="+encodeURIComponent(TargetProperty)+"&value="+encodeURIComponent(value));
 }
 
+
+function dismissSuggestions()
+{
+    var suggestbox = document.getElementById("suggestbox");
+    if(!suggestbox)
+    {
+        return;
+    }
+    suggestbox.parentNode.removeChild(suggestbox);
+}
+
 function displaySuggestions(recipient, suggestions)
 {
+    console.log(suggestions);
+    dismissSuggestions();
+    let suggestbox = document.createElement("div");
+    suggestbox.setAttribute("class","suggestable_box");
+    suggestbox.setAttribute("id","suggestbox");
+    recipient.parentNode.appendChild(suggestbox);
+    for(i =0; i< suggestions.length;i++)
+    {
+        let suggestion = suggestions[i];
+        console.log(suggestion);
+        let entry = document.createElement("a");
+        entry.setAttribute("class","box_suggestion");
+        entry.setAttribute("href","#");
+        entry.dataset.index = i;
+        entry.innerText = suggestion;
+        entry.addEventListener("click",()=>{
+           recipient.value = entry.innerText;
+           dismissSuggestions();
+           console.log(entry.innerText);
+        });
+        suggestbox.appendChild(entry);
+        suggestbox.dataset.count = suggestions.length;
+        suggestbox.dataset.selectedIndex = -1;
+        
+    }
     
 }
 
-function doSuggest()
+function doKeyboardNav(e)
 {
+    var suggestbox = document.getElementById("suggestbox");
+    if(!suggestbox)
+    {
+        if(e.keyCode === 40)
+        {
+            doSuggest(e.target);
+        }
+        return;
+    }
+    var current = suggestbox.dataset.selectedIndex;
+    var maxval = suggestbox.dataset.count;
+    var hitEnter = false;
+    switch(e.keyCode)
+    {
+        case 40: // down
+        {
+            current++;
+            break;
+        }
+        case 38: // up
+        {
+            current--;
+            break;
+        }
+        case 13: // enter
+        {
+            hitEnter = true;
+            e.preventDefault();
+        }
+    }
+    // clamp between -1 and maximum index
+    current = Math.max(-1, Math.min(current, maxval-1));
+    console.log(current);
+    if(current != -1)
+    {
+        document.querySelectorAll(".suggestable_box a").forEach((e)=>{
+            if(e.dataset.index == current)
+            {
+                e.classList.add("selected_suggestion");
+                if(hitEnter)
+                {
+                    e.click();
+                    return;
+                }
+            }
+            else
+            {
+                e.classList.remove("selected_suggestion");
+            }
+            
+        });
+        suggestbox.dataset.selectedIndex = current;
+    }
+    else
+    {
+        dismissSuggestions();
+    }
     
-    let suggestURL = this.dataset.suggestionsource;
+}
+
+function doSuggest(target)
+{
+    let suggestbox = document.getElementById("suggestbox");
+    let suggestURL = target.dataset.suggestionsource;
     
-    var target =this;
     let ajax = new XMLHttpRequest();
     ajax.onreadystatechange=function()
     {
@@ -141,7 +238,7 @@ function doSuggest()
             }
         }
     };
-    ajax.open("GET",suggestURL+encodeURIComponent(this.value),true);
+    ajax.open("GET",suggestURL+encodeURIComponent(target.value),true);
     ajax.send(null);
 }
 
@@ -154,6 +251,9 @@ function AttachPostUpdate()
         e.addEventListener("blur",DoPostUpdate);
         e.readonly=true;
     });
+    console.log("attached post update");
+    document.addEventListener("click",()=>{dismissSuggestions();});
+    
 }
 
 window.addEventListener("load",AttachPostUpdate);
--- templates/pixdb/singlepic.tpl
+++ templates/pixdb/singlepic.tpl
@@ -1,9 +1,9 @@
 <a href="/pixdb/">Back</a>
 <h3>{%w%} x {%h%} </h2>
-<img class="singleimage" src="/files/stream/{%blobid%}/{%blobid%}.{%ext%}" />
 <div class="tags_container">
     {#foreach|{%tags%}|<a href="/pixdb/tag/{:*:}">{:*:}</a> #}
 </div>
 <div class="suggestable_input_container">
-    <input data-suggestionsource="/main/tag/suggest/" data-evaobject="{%id%}" oninput="doSuggest();" onblur="dismissSuggestions();" id="tag_input" name="tag_input" size=20 /><button type="button" onclick="addTag('tag_input');">Add tag</button>
+    <input data-suggestionsource="/main/tag/suggest/" data-evaobject="{%id%}" oninput="doSuggest(this);" onkeydown="doKeyboardNav(event);" onblur="" id="tag_input" name="tag_input" size=20 /><button type="button" onclick="addTag('tag_input');">Add tag</button>
 </div>
+<img class="singleimage" src="/files/stream/{%blobid%}/{%blobid%}.{%ext%}" />
