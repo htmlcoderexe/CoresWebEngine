91b6d9c2d3ff45a285e8a2886a10abcb607ab1db
Valentina <34088724+htmlcoderexe@users.noreply.github.com>
2025-11-30 18:13:52 +0100

    added migration (with no erase)

--- modules/files/main.php
+++ modules/files/main.php
@@ -27,3 +27,41 @@ function ModuleAction_files_stream($params)
     
     
 }
+
+function ModuleAction_files_migrate($params)
+{
+    EngineCore::$DEBUG=true;
+    DBHelper::$DEBUG=true;
+    EngineCOre::RawModeOn();
+    $files = EVA::GetAllOfType('file');
+    //$files=[];
+    $i=0;
+    //var_dump($files);
+    echo "found ".count($files)." to migrate <br />";
+    while($i<count($files))
+    {
+        $batch=[];
+        for($j=0;$j<50;$j++)
+        {
+            
+            if(($j+$i)>=count($files))
+            {
+                break;
+            }
+            $batch[]=$files[$j+$i];
+        }
+        //var_dump($batch);
+        $i+=$j;
+        echo "batch of " . count($batch) ." to process<br />";
+        $table = EVA::GetAsTable(['filename','blobid','hash','file.extension','mimetype','filesize','timestamp'],'file',$batch);
+        foreach($table as $id=>$fileinfo)
+        {
+            DBHelper::Insert(Files_FAT,[
+                null, $fileinfo['blobid'],$fileinfo['timestamp'],$fileinfo['mimetype'],
+                $fileinfo['filesize'],$fileinfo['filename'],$fileinfo['file.extension'],$fileinfo['hash']
+            ]);
+        }
+        flush();
+    }
+    
+}
