b07eb681b0603d1327b9bd08bf10b78a0faa0b32
Valentina <34088724+htmlcoderexe@users.noreply.github.com>
2026-08-09 01:07:20 +0200

    there's the router

--- classes/Router.php
+++ classes/Router.php
@@ -2,8 +2,9 @@
 
 class Router
 {
-    public static $DefaultRoute="main";
-    public static $DefaultAction="default";
+    public static $RouteMap = [];
+    public const DEFAULT_ROUTE = "main";
+    public const DEFAULT_ACTION = "default";
     public static function Dispatch()
     {
         //  URL rewriting converts requests of the form "(example.net)/path/to/something"
@@ -11,7 +12,38 @@ class Router
         // "route" gets populated with the path component
         $query=EngineCore::GET("route");
         //split route into individual segments
-        $pieces=explode("/",$query);//Utility::ddump($pieces);
+        $pieces=$query==''?[]:explode("/",$query);
+        
+        if(count($pieces)<1)
+        {
+            $pieces[]=self::DEFAULT_ROUTE;
+        }
+        if(count($pieces)<2)
+        {
+            $pieces[]=self::DEFAULT_ACTION;
+        }
+        $mapzoom = &self::$RouteMap;
+        while(count($pieces)>0)
+        {
+            $p = array_shift($pieces);
+            if(isset($mapzoom[$p]))
+            {
+                if(is_callable($mapzoom[$p]))
+                {
+                   
+                   return $mapzoom[$p](...$pieces);
+                }
+                else
+                {
+                    $mapzoom = &$mapzoom[$p];
+                    continue;
+                }
+                
+            }
+            //404
+            return false;
+        }
+        
         //first segment should be module name, shift it off
         $modulename=count($pieces)>0?array_shift($pieces):"main"; //munch, munch
         //second segment is action, shift it off
@@ -19,15 +51,65 @@ class Router
         //default route
         if($modulename == "")
         {
-            $modulename = Router::$DefaultRoute;
+            $modulename = Router::DEFAULT_ROUTE;
         }
         // default action
         if($action == "")
         {
-            $action = Router::$DefaultAction;
+            $action = Router::DEFAULT_ACTION;
         }
         $module=new Module($modulename);
         //hand the rest of segments as arguments to module's action. This can be empty
         $module->PerformAction($action,$pieces); //CHOMP!!
+    }
+    
+    public static function AddRoute(string $route, callable $func)
+    {
+        $pieces = explode(separator: "/", string: $route);
+        if(count($pieces)===1)
+        {
+            $pieces[]='default';
+        }
+        // at least 2 pieces
+        // for 3 stage route like /ticket/group/view
+        // should be in map['ticket']['group']['view'] -> callable
+        /*
+         * [
+         *      'main'=>[
+         *          'default'=>callable, 
+         *          'test'=>callable
+         *      ],
+         *      'ticket'=>[
+         *          'view'=>callable, 
+         *          'new'=>callable, 
+         *          'group'=>[
+         *              'create'=>callable,
+         *              'list'=>callable
+         *          ]
+         *      ]
+         */
+        
+        //*/
+        $map = &self::$RouteMap;
+        $level = 0;
+        for($i=0;$i<count($pieces)-1;$i++)
+        {
+            if(!isset($map[$pieces[$i]]))
+            {
+                $map[$pieces[$i]] = [];
+            }
+            if(is_callable($map[$pieces[$i]]))
+            {
+                // failed
+                return false;
+            }
+            $map = &$map[$pieces[$i]];
+        }
+        if(isset($map[$pieces[count($pieces)-1]]))
+        {
+            return false;
+        }
+        $map[$pieces[count($pieces)-1]] = $func;
+        return true;
     }
 }
--- index.php
+++ index.php
@@ -28,8 +28,41 @@ require_once CLASS_DIR."DBHelper.php";
 require_once CLASS_DIR."TemplateProcessor.php";
 require_once CLASS_DIR."User/UserExtendedProps.php";
 require_once CLASS_DIR."EVA.php";
+
+
+
 spl_autoload_register(function ($class) {
-    require_once CLASS_DIR. $class . '.php';
+    
+    $mapping = [
+        'Models'=>'models',
+        'ViewModels'=>'viewmodels'
+    ];
+    if(file_exists(CLASS_DIR. $class . '.php'))
+    {
+        require_once CLASS_DIR. $class . '.php';
+        return;
+    }
+    
+    $path = explode(string: $class, separator: "\\");
+    if($path[0]=="")
+    {
+        array_shift($path);
+    }
+    $type = array_shift($path);
+    $prefix = "";
+    if(isset($mapping[$type]))
+    {
+        $prefix = $mapping[$type];
+    }
+    $fullpath = $prefix.DIRECTORY_SEPARATOR.implode(array: $path, separator: DIRECTORY_SEPARATOR).".php";
+    if(file_exists($fullpath))
+    {
+        require_once($fullpath);
+    }
+    else
+    {
+        throw new Exception("Unable to load [$class] from [$fullpath]");
+    }
 });
 header("Content-Security-Policy:  frame-ancestors 'self' ".BASE_URI);
 ini_set("session.cache_limiter","");
@@ -37,12 +70,66 @@ ini_set('xdebug.var_display_max_depth', 10);
 ini_set('xdebug.var_display_max_children', 256);
 ini_set('xdebug.var_display_max_data', 1024);
 session_start();
-EngineCore::$CurrentUser=User::GetCurrentUser();
+EngineCore::$CurrentUser=\User::GetCurrentUser();
 $_PAGE_SIDEBAR=Array();
 require_once CLASS_DIR."Router.php";
+
+#[\Attribute]
+class Route
+{
+    public function __construct(public string $path){}
+}
+#[\Attribute]
+class View
+{
+    public function __construct(public string $name){}
+}
+
+
+foreach(glob("controllers/*.php") as $filename)
+{
+    require_once $filename;
+    $classname = substr(string: $filename,offset: 12,length:-4);
+    $ref = new \ReflectionClass("Controllers\\".$classname);
+    $funcs = $ref->getMethods();
+    foreach($funcs as $func)
+    {
+        $routes = $func->getAttributes("Route");
+        foreach($routes as $route)
+        {
+            $r = $route->newInstance();
+            Router::AddRoute($r->path, $func->getClosure());
+        }
+    }
+}
+//die;
 EngineCore::StartLap();
 $time = microtime();
-Router::Dispatch();
+$result = Router::Dispatch();
+if(!$result)
+{
+    die("404");
+}
+
+$viewname = $result['entity_type'];
+$accepts = \HTTPHeaders::GetAccepts($_SERVER['HTTP_ACCEPT']);
+$mime = $accepts[0]['mime'];
+switch($mime)
+{
+    case "text/html":
+    {
+        $tpl = new TemplateProcessor($viewname,false,'views/');
+        $tpl->tokens = $result;
+        EngineCore::SetPageContent($tpl->process(true));
+        break;
+    }
+    case "application/json":
+    {
+        EngineCore::EmitJSON($result);
+        break;
+    }
+}
+
 
 EngineCore::Write2Debug("<strong>Route:</strong>".EngineCore::GET("route"));
 
